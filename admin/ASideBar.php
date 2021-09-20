1fb2d3a96da658172cd81f150e764d6a9cdb7676
Jason Low <jasonlowxd@gmail.com>
2021-09-20 19:16:44 +0800

    admin new file

---
+++ admin/ASideBar.php
@@ -0,0 +1,31 @@
+<aside class="left-sidebar">
+    <!-- Sidebar scroll-->
+    <div class="scroll-sidebar">
+        <!-- Sidebar navigation-->
+        <nav class="sidebar-nav">
+            <ul id="sidebarnav">
+                <li class="nav-small-cap">--- MANAGEMENT</li>
+                <li> <a class="waves-effect waves-dark" href="Aindex.php"><i class="icon-calender"></i><span class="hide-menu">Calendar</span></a>
+                </li>
+                <li> <a class="waves-effect waves-dark" href="Ateacher.php"><i class="icon-people"></i><span class="hide-menu">Teacher</span></a>
+                </li>
+                <li> <a class="waves-effect waves-dark" href="javascript:void(0)"><i class="icon-user"></i><span class="hide-menu">Parent</span></a>
+                </li>
+                <li> <a class="waves-effect waves-dark" href="javascript:void(0)"><i class="ti-credit-card"></i><span class="hide-menu">Invoice</span></a>
+                </li>
+                <li> <a class="waves-effect waves-dark" href="javascript:void(0)"><i class="ti-receipt"></i><span class="hide-menu">Payment Receipt</span></a>
+                </li>
+                <li> <a class="waves-effect waves-dark" href="javascript:void(0)"><i class="icon-folder"></i><span class="hide-menu">Learning Resource</span></a>
+                </li>
+                <li> <a class="waves-effect waves-dark" href="javascript:void(0)"><i class="icon-graduation"></i><span class="hide-menu">Student Progression</span></a>
+                </li>
+
+                <li class="nav-small-cap">--- SETTINGS</li>
+                <li> <a class="waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false"><i class="icon-settings text-info"></i><span class="hide-menu">Account Settings</span></a></li>
+                <li> <a class="waves-effect waves-dark" href="../index.php" aria-expanded="false"><i class="icon-logout text-danger"></i><span class="hide-menu">Log Out</span></a></li>
+            </ul>
+        </nav>
+        <!-- End Sidebar navigation -->
+    </div>
+    <!-- End Sidebar scroll-->
+</aside>
---
+++ admin/ATopbar.php
@@ -0,0 +1,114 @@
+<header class="topbar">
+    <nav class="navbar top-navbar navbar-expand-md navbar-dark">
+        <!-- ============================================================== -->
+        <!-- Logo -->
+        <!-- ============================================================== -->
+        <div class="navbar-header">
+            <a class="navbar-brand" href="Aindex.php">
+                <!-- Logo icon -->
+                <b class="m-l-5 m-r-5">
+                    <!--You can put here icon as well // <i class="wi wi-sunset"></i> //-->
+                    <i class="fa fa-music"></i>
+                    <!-- Dark Logo icon -->
+                    <!-- <img src="../assets/images/logo-icon.png" alt="homepage" class="dark-logo" /> -->
+                    <!-- Light Logo icon -->
+                    <!-- <img src="../assets/images/logo-light-icon.png" alt="homepage" class="light-logo" /> -->
+                </b>
+                <!--End Logo icon -->
+                <span class="hidden-xs"><span class="font-bold">MUSIC</span> ACADEMY</span>
+            </a>
+        </div>
+        <!-- ============================================================== -->
+        <!-- End Logo -->
+        <!-- ============================================================== -->
+        <div class="navbar-collapse">
+            <!-- ============================================================== -->
+            <!-- toggle and nav items -->
+            <!-- ============================================================== -->
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item"> <a class="nav-link nav-toggler d-block d-md-none waves-effect waves-dark" href="javascript:void(0)"><i class="ti-menu"></i></a> </li>
+                <li class="nav-item"> <a class="nav-link sidebartoggler d-none d-lg-block d-md-block waves-effect waves-dark" href="javascript:void(0)"><i class="icon-menu"></i></a> </li>
+            </ul>
+            <!-- ============================================================== -->
+            <!-- User profile -->
+            <!-- ============================================================== -->
+            <ul class="navbar-nav my-lg-0">
+                <!-- ============================================================== -->
+                <!-- Notifications -->
+                <!-- ============================================================== -->
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle waves-effect waves-dark" href="" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="ti-bell"></i>
+                        <div class="notify"> <span class="heartbit"></span> <span class="point"></span> </div>
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-right mailbox animated bounceInDown">
+                        <ul>
+                            <li>
+                                <div class="drop-title">Notifications</div>
+                            </li>
+                            <li>
+                                <div class="message-center">
+                                    <!-- Message -->
+                                    <a href="javascript:void(0)">
+                                        <div class="btn btn-danger btn-circle"><i class="fa fa-link"></i></div>
+                                        <div class="mail-contnet">
+                                            <h5>Luanch Admin</h5> <span class="mail-desc">Just see the my new admin!</span> <span class="time">9:30 AM</span>
+                                        </div>
+                                    </a>
+                                    <!-- Message -->
+                                    <a href="javascript:void(0)">
+                                        <div class="btn btn-success btn-circle"><i class="ti-calendar"></i></div>
+                                        <div class="mail-contnet">
+                                            <h5>Event today</h5> <span class="mail-desc">Just a reminder that you have event</span> <span class="time">9:10 AM</span>
+                                        </div>
+                                    </a>
+                                    <!-- Message -->
+                                    <a href="javascript:void(0)">
+                                        <div class="btn btn-info btn-circle"><i class="ti-settings"></i></div>
+                                        <div class="mail-contnet">
+                                            <h5>Settings</h5> <span class="mail-desc">You can customize this template as you want</span> <span class="time">9:08 AM</span>
+                                        </div>
+                                    </a>
+                                    <!-- Message -->
+                                    <a href="javascript:void(0)">
+                                        <div class="btn btn-primary btn-circle"><i class="ti-user"></i></div>
+                                        <div class="mail-contnet">
+                                            <h5>Pavan kumar</h5> <span class="mail-desc">Just see the my admin!</span> <span class="time">9:02 AM</span>
+                                        </div>
+                                    </a>
+                                </div>
+                            </li>
+                            <li>
+                                <a class="nav-link text-center link" href="javascript:void(0);"> <strong>Check all notifications</strong> <i class="fa fa-angle-right"></i> </a>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+                <!-- ============================================================== -->
+                <!-- End Notifications -->
+                <!-- ============================================================== -->
+                <!-- ============================================================== -->
+                <!-- User Profile -->
+                <!-- ============================================================== -->
+                <li class="nav-item dropdown u-pro">
+                    <a class="nav-link dropdown-toggle waves-effect waves-dark profile-pic" href="" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <span class="hidden-md-down">Admin &nbsp;
+                            <i class="fa fa-angle-down"></i>
+                        </span>
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-right animated flipInY">
+                        <!-- text-->
+                        <a href="javascript:void(0)" class="dropdown-item"><i class="ti-settings"></i> Account Setting</a>
+                        <!-- text-->
+                        <div class="dropdown-divider"></div>
+                        <!-- text-->
+                        <a href="../index.php" class="dropdown-item"><i class="fa fa-power-off"></i> Logout</a>
+                        <!-- text-->
+                    </div>
+                </li>
+                <!-- ============================================================== -->
+                <!-- End User Profile -->
+                <!-- ============================================================== -->
+            </ul>
+        </div>
+    </nav>
+</header>
