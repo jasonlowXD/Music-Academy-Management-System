3f9e17687d33c2a856894e1d1fb1a7e99f7bf64c
Jason Low <jasonlowxd@gmail.com>
2022-01-11 18:55:39 +0800

    added check parent status condition

--- admin/editChild.php
+++ admin/editChild.php
@@ -15,7 +15,7 @@ $conn = mysqli_connect("localhost", "root", "", "music_academy");
 
 if ($conn) {
 
-    $sql = "SELECT * FROM CHILD WHERE CHILD_ID = '$child_id'";
+    $sql = "SELECT * FROM CHILD LEFT JOIN PARENT ON CHILD.PARENT_ID = PARENT.PARENT_ID WHERE CHILD_ID = '$child_id'";
     $result = $conn->query($sql);
     while ($row = $result->fetch_assoc()) {
         $db_child_name = $row["CHILD_NAME"];
@@ -23,6 +23,7 @@ if ($conn) {
         $db_child_status = $row["CHILD_STATUS"];
         $db_child_teacher = $row["TEACHER_ID"];
         $db_child_course = $row["COURSE_ID"];
+        $db_child_parent = $row["PARENT_STATUS"];
     }
 
     // IF NOTHING CHANGE
@@ -34,16 +35,23 @@ if ($conn) {
 
     // IF GOT CHANGE, UPDATE CHILD
     else {
-        $sql2 = "UPDATE CHILD SET CHILD_NAME ='$childName', CHILD_AGE = '$childAge', CHILD_STATUS = '$childStatus', TEACHER_ID = '$childTeacher' , COURSE_ID = '$childCourse' WHERE CHILD_ID = '$child_id' ";
-
-        if (mysqli_query($conn, $sql2)) {
-            $response['title']  = 'Done!';
-            $response['status']  = 'success';
-            $response['message'] = 'Child edited!';
-        } else {
+        // IF PARENT IS INACTIVE, ALL CHILDREN CANNOT UPDATE INFO
+        if ($db_child_parent == 'inactive') {
             $response['title']  = 'Error!';
             $response['status']  = 'error';
-            $response['message'] = 'mysql error';
+            $response['message'] = 'Child cannot update due to inactive parent!';
+        } else {
+            $sql2 = "UPDATE CHILD SET CHILD_NAME ='$childName', CHILD_AGE = '$childAge', CHILD_STATUS = '$childStatus', TEACHER_ID = '$childTeacher' , COURSE_ID = '$childCourse' WHERE CHILD_ID = '$child_id' ";
+
+            if (mysqli_query($conn, $sql2)) {
+                $response['title']  = 'Done!';
+                $response['status']  = 'success';
+                $response['message'] = 'Child edited!';
+            } else {
+                $response['title']  = 'Error!';
+                $response['status']  = 'error';
+                $response['message'] = 'mysql error';
+            }
         }
     }
 } else {
